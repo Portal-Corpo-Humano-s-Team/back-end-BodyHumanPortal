041d9fbf6cdd92772ccadffb410fd13ffd63fca2
matheusmichalski <matheus.michalskifc@gmail,com>
2025-01-19 00:16:00 -0300

    Type: Type validateEmail

--- src/middleware/validateEmail.ts
+++ src/middleware/validateEmail.ts
@@ -3,9 +3,10 @@ import { IAuthRequest } from "../types/authTypes.ts";
 import { ValidationError } from "../validations/CustomValidation.ts";
 import { IUser } from "../types/userTypes.ts";
 import { validateEmailDomain } from "../validations/validationEmail.ts";
+
 export default async function validateEmailRequest(req: IAuthRequest, res: Response, next: NextFunction) {
   try {
-    const { email } = req.body.email ? req.body : req.user || {};
+    const { email }: IUser = req.body.email ? req.body : req.user || {};
     if (!email) {
       throw new ValidationError("Email not provided");
     }
